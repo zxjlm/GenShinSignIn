4fbb88dd772b8aab514708276f25420adbc09842
harumonia <zxjlm233@gmail.com>
2021-09-26 17:53:40 +0800

    🚀 add server less

---
+++ serverless.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import logging
+from utils import get_python_version
+import glob
+import setting
+from main import process
+
+# if you open the initializer feature, please implement the initializer function, as below:
+# def initializer(context):
+#   logger = logging.getLogger()
+#   logger.info('initializing')
+
+
+def handler(event, context):
+    logger = logging.getLogger()
+    logger.info(get_python_version())
+    config_files = glob.glob(setting.path + '/config/config_*.json')
+    for config_file in config_files:
+        logger.info(
+            '****************** start to get config {} *********', config_file)
+        process(config_file)
+    # logger.info('hello world')
+    # return 'hello world'
